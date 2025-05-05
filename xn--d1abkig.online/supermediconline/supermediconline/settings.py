3ce9c430b3376799a00f871f54184e3ecb7425f6
63phc <pavel.burns@gmail.com>
2025-05-05 13:55:35 +0200

    pg

--- xn--d1abkig.online/supermediconline/supermediconline/settings.py
+++ xn--d1abkig.online/supermediconline/supermediconline/settings.py
@@ -34,16 +34,12 @@ else:
     ]
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.mysql",
+            "ENGINE": "django.db.backends.postgresql",
             "NAME": os.getenv("DB_NAME"),
             "USER": os.getenv("DB_USER"),
             "PASSWORD": os.getenv("DB_PASSWORD"),
             "HOST": os.getenv("DB_HOST"),
-            "PORT": "",
-            "OPTIONS": {
-                "charset": "utf8mb4",
-                "sql_mode": "STRICT_ALL_TABLES",
-            },
+            "PORT": os.getenv("DB_PORT", "5432"),
         }
     }
 
